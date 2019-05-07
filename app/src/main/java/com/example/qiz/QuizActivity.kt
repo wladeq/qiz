bfe754c44d3746bd63c4c9da02d8971efebf9463
wl <v.ignatenko@fairplacefinance.pl>
2019-05-07 19:15:28 +0200

    second

---
+++ app/src/main/java/com/example/qiz/QuizActivity.kt
@@ -0,0 +1,12 @@
+package com.example.qiz
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class QuizActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
